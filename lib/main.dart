fa394415dbc91c6eeecc02a63cfadc8c78aae5cf
jose.auza <jose.auza@gmail.com>
2024-04-03 19:27:23 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screens/home/home.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+// sandbox
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: const Text('sandbox')
+    );
+  }
+}
